de414c49567797f6ca31e7d51fc6cb4ad9e67005
Narayandas Kachhi <126740162+narayan24x7@users.noreply.github.com>
2025-11-19 02:43:40 +0000

    Init commit

---
+++ Iteration/01.js
@@ -0,0 +1,50 @@
+// For
+for (let i = 0; i < 10; i++) {
+    const element = i;
+    if (element == 5) {
+        // console.log("5 is best number");
+    }
+    // console.log(element);
+}
+
+// console.log(element);
+    
+for (let i = 0; i <=10; i++) {
+    //  console.log(`Outer loop value: ${i}`);
+    
+    for (let j = 0; j <= 10; j++) {
+        // console.log(`Inner loop value: ${j} and Outer loop value: ${i}`);
+        // console.log(i + '*' + j + '=' + i*j);
+        
+    } 
+}
+
+let myArray=["flash","ironman","batman"]
+// console.log(myArray=Array.length);
+
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    // console.log(element);
+    
+}
+
+// brake and continue
+
+
+for (let index = 1; index <= 20; index++) {
+    if (index == 5) {
+        // console.log("5 is best");
+        break;
+    }
+   // console.log(`Value is ${index}`);
+   
+}
+
+for (let index = 1; index <= 20; index++) {
+    if (index == 5) {
+        console.log("5 is best");
+        continue;
+    }
+   console.log(`Value is ${index}`);
+}
+
